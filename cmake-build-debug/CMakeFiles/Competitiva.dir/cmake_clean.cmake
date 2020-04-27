7d1ccb9cb824a7deb903dbf11b3cd209decf7095
Lucas Moreno <lucas.moreno.olivos@gmail.com>
2020-04-27 14:19:17 -0500

    first commit

---
+++ cmake-build-debug/CMakeFiles/Competitiva.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Competitiva.dir/main.cpp.o"
+  "Competitiva"
+  "Competitiva.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Competitiva.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
